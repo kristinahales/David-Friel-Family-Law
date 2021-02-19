e5beebb5575d1e11c2412700cff07090201c255c
Kristina Hales <krissyhales22@churchofjesuschrist.org>
2021-02-19 08:41:49 -0700

    changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const {SERVER_PORT, EMAIL_USERNAME, EMAIL_PASSWORD, NOTIFICATION_FROM, NOTIFICATION_LIST} = process.env
+const { EMAIL_USERNAME, EMAIL_PASSWORD, NOTIFICATION_FROM, NOTIFICATION_LIST} = process.env
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,8 +21,13 @@ app.use(express.static('client/build'));
 // serve up the index.html if express does'nt recognize the route
 const path = require('path');
 app.get('*', (req, res) => {
-res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+res.sendFile(path.resolve('src', 'header', 'Header.js'));
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`)
+})
 
 
 app.post('/api/form', (req, res) => {
